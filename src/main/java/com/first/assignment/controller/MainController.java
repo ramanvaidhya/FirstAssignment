cd67fb60792a9ed7d416806f36afaf862e9d6846
ramanvaidhya <ramanvaidhya@gmail.com>
2014-11-14 11:02:47 +0530

    Initial Project Structure and Additions

---
+++ src/main/java/com/first/assignment/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.first.assignment.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+
+}
